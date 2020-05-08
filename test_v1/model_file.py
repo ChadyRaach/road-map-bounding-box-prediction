fa81be724ec68f8ecf35fd68c366f3c48cea060d
craach <59753069+craach@users.noreply.github.com>
2020-05-08 16:38:22 -0400

    Update model_file.py

---
+++ test_v1/model_file.py
@@ -0,0 +1,41 @@
+
+class RoadModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.image_model = torch.hub.load('pytorch/vision:v0.6.0', 'resnet18', 
+                                          pretrained=False)
+        self.fc1 = nn.Linear(6000, 2500)
+        self.ups1 = nn.Upsample(size=100, mode='bilinear')
+        self.deconv1 = nn.ConvTranspose2d(in_channels=1, out_channels=32, 
+                                          kernel_size=3, stride=1, padding=1)
+        self.ups2 = nn.Upsample(size=200, mode='bilinear')
+        self.deconv2 = nn.ConvTranspose2d(in_channels=32, out_channels=16, 
+                                          kernel_size=3, stride=1, padding=1)
+        self.ups3 = nn.Upsample(size=400, mode='bilinear')
+        self.deconv3 = nn.ConvTranspose2d(in_channels=16, out_channels=4, 
+                                          kernel_size=3, stride=1, padding=1)
+        self.ups4 = nn.Upsample(size=800, mode='bilinear')
+        self.deconv4 = nn.ConvTranspose2d(in_channels=4, out_channels=1, 
+                                          kernel_size=3, stride=1, padding=1)
+    
+    
+    def forward(self, x):
+        features = []
+        for im in x:
+            features.append(self.image_model(im))
+        x = torch.cat(features, dim=1)
+        x = self.fc1(x)
+        # x = F.relu(x)
+        x = x.reshape([-1, 1, 50, 50])
+        x = self.ups1(x)
+        x = self.deconv1(x)
+        x = F.relu(x)
+        x = self.ups2(x)
+        x = self.deconv2(x)
+        x = F.relu(x)
+        x = self.ups3(x)
+        x = self.deconv3(x)
+        x = F.relu(x)
+        x = self.ups4(x)
+        x = self.deconv4(x)
+        return F.sigmoid(x)
