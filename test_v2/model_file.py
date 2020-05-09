12cf9a8c0d2f849b9be857a120fe8d611cd9fdad
Jatin Khilnani <Jatin.Khilnani@nyu.edu>
2020-05-08 22:09:23 -0400

    Test

---
+++ test_v2/model_file.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+
+# The full road model holds and image model (ResNet18), and applies that
+# image model to each of the 6 road images.
+class RoadModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.image_model = torch.hub.load('pytorch/vision:v0.6.0', 'resnet18', 
+                                          pretrained=False)
+        self.fc1 = nn.Linear(6000, 2500)
+        self.ups1 = nn.Upsample(size=100, mode='bilinear')
+        self.deconv1 = nn.ConvTranspose2d(in_channels=1, out_channels=32, 
+                                          kernel_size=3, stride=1, padding=1)
+        self.ups2 = nn.Upsample(size=200, mode='bilinear')
+        self.deconv2 = nn.ConvTranspose2d(in_channels=32, out_channels=16, 
+                                          kernel_size=3, stride=1, padding=1)
+        self.ups3 = nn.Upsample(size=400, mode='bilinear')
+        self.deconv3 = nn.ConvTranspose2d(in_channels=16, out_channels=4, 
+                                          kernel_size=3, stride=1, padding=1)
+        self.ups4 = nn.Upsample(size=800, mode='bilinear')
+        self.deconv4 = nn.ConvTranspose2d(in_channels=4, out_channels=1, 
+                                          kernel_size=3, stride=1, padding=1)
+    
+    
+    def forward(self, x):
+        features = []
+        for im in x:
+            features.append(self.image_model(im))
+        x = torch.cat(features, dim=1)
+        x = self.fc1(x)
+        # x = F.relu(x)
+        x = x.reshape([-1, 1, 50, 50])
+        x = self.ups1(x)
+        x = self.deconv1(x)
+        x = F.relu(x)
+        x = self.ups2(x)
+        x = self.deconv2(x)
+        x = F.relu(x)
+        x = self.ups3(x)
+        x = self.deconv3(x)
+        x = F.relu(x)
+        x = self.ups4(x)
+        x = self.deconv4(x)
+        return F.sigmoid(x)
+    
+
+# Bounding Box Predictor
+class BoundingBoxModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+    
+    
+    def forward(self, x):
+        pass
---
+++ test_v2/model_loader.py
@@ -0,0 +1,61 @@
+"""
+You need to implement all four functions in this file and also put your team info as a variable
+Then you should submit the python file with your model class, the state_dict, and this file
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+
+# import your model class
+from model_file import RoadModel, BoundingBoxModel
+
+# Set up your device 
+cuda = torch.cuda.is_available()
+device = torch.device("cuda:0" if cuda else "cpu")
+
+# Put your transform function here, we will use it for our dataloader
+# For bounding boxes task
+def get_transform_task1(): 
+    return torchvision.transforms.ToTensor()
+# For road map task
+def get_transform_task2(): 
+    return torchvision.transforms.ToTensor()
+
+class ModelLoader():
+    # Fill the information for your team
+    team_name = 'team_name'
+    team_number = 1
+    round_number = 3
+    team_member = ['Henry Steinitz', 'Chady Raach', 'Jatin Khilnani']
+    contact_email = '@nyu.edu'
+
+    def __init__(self, model_file='model_state_dict.pt'):
+        # You should 
+        #       1. create the model object
+        ri_model = RoadModel()
+        bb_model = BoundingBoxModel()
+        #       2. load your state_dict
+        model_state_dict = torch.load(model_file)
+        ri_model.load_state_dict(model_state_dict['ri_model'])
+        bb_model.load_state_dict(model_state_dict['bb_model'])
+        #       3. call cuda()
+        self.ri_model = ri_model.to(device)
+        self.bb_model = bb_model.to(device)
+        # 
+        # pass
+
+    def get_bounding_boxes(self, samples):
+        # samples is a cuda tensor with size [batch_size, 6, 3, 256, 306]
+        # You need to return a tuple with size 'batch_size' and each element is a cuda tensor [N, 2, 4]
+        # where N is the number of object
+
+        return (torch.rand(1, 15, 2, 4) * 80) - 40
+
+    def get_binary_road_map(self, samples):
+        # samples is a cuda tensor with size [batch_size, 6, 3, 256, 306]
+        # You need to return a cuda tensor with size [batch_size, 800, 800] 
+        
+        # return torch.rand(1, 800, 800) > 0.5
+        return self.ri_model(samples.permute(1,0,2,3,4)) > 0.5
